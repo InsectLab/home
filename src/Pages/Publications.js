5cdb15c6e0d02477b4e8e94230df9c2f62ca88e8
Roei-Be <r.balisha@gmail.com>
2023-08-14 16:43:33 +0300

    Added pblications page with data from DB

--- src/Pages/Publications.js
+++ src/Pages/Publications.js
@@ -1,22 +1,39 @@
-import { toBibtex } from "bibtex-parse-js";
 import {BibtexParser} from "bibtex-js-parser";
 
+import Article from "../assets/Article";
+
+import { ThemeContext } from "../context/ThemeContext";
 import { DBContext } from "../context/DBContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+
+import "../CSS/Publications.css"
 
 const Publications = () => {
 
+    const { darkMode } =useContext(ThemeContext);
     const {members} = useContext(DBContext);
+    const [Publications, setPublications] = useState("");
+
+    useEffect(() => {
+        members && setPublications(BibtexParser.parseToJSON(members[2].Publications));
+        /* member && console.log(member.Publications); */
+        members && console.log(BibtexParser.parseToJSON(members[2].Publications));
+    }, [members])
 
     return (
-        <div>
-            {members && members.map((member) => {
-                return (
-                    <div>
-                        
-                    </div>
-                )
-            })}
+        <div className="publicaionsWrraper">
+            <h1>Publications</h1>
+            <div className="articlesListWrraper">
+                {Publications && Publications.map((article) => {
+                    return (
+                        <div className="articleItem">
+                            <span className="articleTitle">{<Article color={`${darkMode ? "white" : "black"}`}/>}{article.title}</span>
+                            <span className="articleAuthor">{article.author}</span>
+                            <span>{article.publisher && (`${article.publisher}  -  `)}{article.year}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
