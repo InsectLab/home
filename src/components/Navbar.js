9845e2a326dbe6482232aa4a56213b62671251f2
roei-be <r.balisha@gmail.com>
2023-08-20 21:22:14 +0300

    added logos to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,8 +22,8 @@ const Navbar = () => {
     }
     return (
         <div className={`${darkMode ? "navbar navbarDark" : "navbar"}`}>
-            <div className="logo"><Link to="https://english.tau.ac.il/"><img src="https://static.wixstatic.com/media/56112d_1efe4d20db6249f1a5876256376aabbc~mv2.gif" alt="1" /></Link></div>
-            <div className="logo"><img src='https://nano.tau.ac.il/sites/shtans1-english.tau.ac.il/files/media_server/Nano/Summer%20School/Tel%20Aviv%20University%20%20-%20Northwestern%20University%20Nano%20Initiative%20-%20Logo.png'></img></div>
+            <div className="logo"><Link to="/"><img src="https://static.wixstatic.com/media/56112d_1efe4d20db6249f1a5876256376aabbc~mv2.gif" alt="1" /></Link></div>
+            <div className="logo"><img src='https://www.aftau.org/wp-content/uploads/2020/07/Wise.png'></img></div>
             <div className="links">
                 <NavLink to={"/"}><span>Home</span></NavLink>
                 <NavLink to={"/reserch"}><span>Reserch</span></NavLink>
@@ -33,7 +33,7 @@ const Navbar = () => {
                 <NavLink to={"/positions"}><span>Open posiotions</span></NavLink>
             </div>
             <>
-                <div className="logo"><img src='https://www.aftau.org/wp-content/uploads/2020/07/Wise.png'></img></div>
+                <div className="logo"><Link to="https://english.tau.ac.il/" target="_blank"><img src='https://nano.tau.ac.il/sites/shtans1-english.tau.ac.il/files/media_server/Nano/Summer%20School/Tel%20Aviv%20University%20%20-%20Northwestern%20University%20Nano%20Initiative%20-%20Logo.png'></img></Link></div>
                 <div className='darkModeSwitch' onClick={darkModeSwitch}>
                     <span>{darkMode ? <LightMode color={"white"}/> : <DarkMode color={"black"}/>}</span>
                 </div>
